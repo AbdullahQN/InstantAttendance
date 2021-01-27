ff9d7aadc8151e744d8fbf5511c6cbfcdc4c90a7
Abod-PC <inficted4@gmail.com>
2021-01-27 04:13:26 +0300

    span courses 2

--- app/src/main/java/com/example/instantattendance/MainActivity.java
+++ app/src/main/java/com/example/instantattendance/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.instantattendance;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -28,10 +34,26 @@ public class MainActivity extends AppCompatActivity {
             uAuth = FirebaseAuth.getInstance();
             FirebaseUser currentUser = uAuth.getCurrentUser();
             if(currentUser==null){
+                //no user is logged in move to login activity
                 Toast.makeText(this,"no user logged in", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(this, signIn.class);
                 startActivity(intent);
             }else{
+                // figure out what type of a user is logged in
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                String forignkey = uAuth.getUid();
+                DocumentReference refrenceFK = db.collection("Users").document(forignkey);
+                refrenceFK.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task){
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+
+                            }
+                        }
+                    }
+                });
                 Intent intent = new Intent(this, HomeActivity.class);
                 startActivity(intent);
                 Toast.makeText(this, uAuth.getCurrentUser().getEmail().toString()+" is signed in", Toast.LENGTH_LONG).show();
